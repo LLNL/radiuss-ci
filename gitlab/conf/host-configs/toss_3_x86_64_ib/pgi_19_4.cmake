81cd43eb2b0555170af575d576459e9a1dc47d42
David Beckingsale <davidbeckingsale@gmail.com>
2020-02-27 16:42:18 -0800

    Add some new configs

---
+++ gitlab/conf/host-configs/toss_3_x86_64_ib/pgi_19_4.cmake
@@ -0,0 +1,9 @@
+##############################################################################
+# Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
+# project contributors. See the COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (MIT)
+##############################################################################
+set(CMAKE_C_COMPILER "/usr/tce/packages/pgi/pgi-19.4/bin/pgcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/pgi/pgi-19.4/bin/pgc++" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/pgi/pgi-19.4/bin/pgfortran" CACHE PATH "")
