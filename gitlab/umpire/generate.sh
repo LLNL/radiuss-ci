04ec81e1f14dba91afd0e3240d40de7d301e481b
Adrien M. Bernede <bernede1@llnl.gov>
2020-04-06 22:36:20 -0700

    First attempt

---
+++ gitlab/umpire/generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+machine=${1:-""}
+machines="butte quartz lassen"
+
+# Build
+if [[ -z ${option} ]]
+then
+    echo "Script needs one parameter: machine name"
+elif [[ ! ${machines} =~ (^|[[:space:]])${machine}($|[[:space:]]) ]]
+then
+    echo "Machine specified unknown"
+    exit 1
+fi
+
+cp child-pipeline.yml ${machine}*.yml ${CI_PROJECT_DIR}
