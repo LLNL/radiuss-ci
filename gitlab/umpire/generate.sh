c88490eac199f8b0102f4e544d981a0840c16bfe
Adrien M. Bernede <bernede1@llnl.gov>
2020-04-07 09:40:37 -0700

    Merging yml files into one, in correct order

--- gitlab/umpire/generate.sh
+++ gitlab/umpire/generate.sh
@@ -7,5 +7,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
 
 for file in *.yml
 do
-    cp ${file} ${CI_PROJECT_DIR}/radiuss-${file}
+    cat ${file} >> ${CI_PROJECT_DIR}/radiuss-umpire-ci.yml
 done
