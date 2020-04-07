90ace1c8b5219723c23ed84da0e817f9a40512ff
Adrien M. Bernede <bernede1@llnl.gov>
2020-04-07 09:03:45 -0700

    Simplified pipeline "generator"

--- gitlab/umpire/generate.sh
+++ gitlab/umpire/generate.sh
@@ -5,17 +5,4 @@ set -o nounset
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-machine=${1:-""}
-machines="butte quartz lassen"
-
-# Build
-if [[ -z ${machine} ]]
-then
-    echo "Script needs one parameter: machine name"
-elif [[ ! ${machines} =~ (^|[[:space:]])${machine}($|[[:space:]]) ]]
-then
-    echo "Machine specified unknown"
-    exit 1
-fi
-
-cp ${DIR}/child-pipeline.yml ${DIR}/${machine}*.yml ${CI_PROJECT_DIR}
+cp ${DIR}/*.yml ${CI_PROJECT_DIR}
