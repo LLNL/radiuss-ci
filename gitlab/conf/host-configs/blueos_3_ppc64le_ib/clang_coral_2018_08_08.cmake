5cb2742078742d8b73d6b559e43349e21b22c96a
Adrien M. Bernede <bernede1@llnl.gov>
2021-08-17 15:21:51 -0700

    The big clean-up

--- gitlab/conf/host-configs/blueos_3_ppc64le_ib/clang_coral_2018_08_08.cmake
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# Copyright (c) 2019, Lawrence Livermore National Security, LLC and other
-# RADIUSS-CI project contributors. See top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: MIT
-###############################################################################
-
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-coral-2018.08.08/bin/clang++" CACHE PATH "")
--- gitlab/conf/host-configs/toss_3_x86_64_ib/clang_9_0_0.cmake
+++
@@ -1,9 +0,0 @@
-###############################################################################
-# Copyright (c) 2019, Lawrence Livermore National Security, LLC and other
-# RADIUSS-CI project contributors. See top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: MIT
-###############################################################################
-
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-9.0.0/bin/clang++" CACHE PATH "")
-set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-9.0.0/bin/clang" CACHE PATH "")
